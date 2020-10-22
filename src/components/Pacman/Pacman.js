cab0085850b920a832a5194428677f9e0e1f7fb8
brudek <brudek@op.pl>
2020-10-22 13:40:49 +0200

    moving pacman part 1

--- src/components/Pacman/Pacman.js
+++ src/components/Pacman/Pacman.js
@@ -23,6 +23,76 @@ class Pacman extends Component {
 
     handleKeyDown = (e) => {
         console.log(e.keyCode, e.key);
+
+        const currentTop = this.state.position.top;
+        const currentLeft = this.state.position.left;
+    
+        // 39 ArrowRight
+        // 40 ArrowDown
+        // 37 ArrowLeft
+        // 38 ArrowUp
+
+        if(e.key === 'ArrowUp'){
+            if(this.state.direction === 'up'){
+                this.setState({
+                    position:{
+                        top: currentTop - 20,
+                        left: currentLeft
+                    }
+                })
+            }
+            else{
+                this.setState({
+                    direction: 'up'
+                })
+            }
+           
+        }
+        else if(e.key === 'ArrowDown'){
+            if(this.state.direction === 'down'){
+                this.setState({
+                    position:{
+                        top: currentTop + 20,
+                        left: currentLeft
+                    }
+                }) 
+            }
+            else{
+                this.setState({
+                    direction: 'down'
+                })
+            }
+        } 
+        else if(e.key === 'ArrowLeft'){
+            if(this.state.direction === 'left'){
+                this.setState({
+                    position: {
+                        top: currentTop,
+                        left: currentLeft - 20
+                    }
+                })
+            }
+            else{
+                this.setState({
+                    direction: 'left'
+                })
+            }
+        }
+        else if(e.key === 'ArrowRight'){
+            if(this.state.direction === 'right'){
+                this.setState({
+                    position: {
+                        top: currentTop,
+                        left: currentLeft + 20
+                    }
+                })
+            }
+            else{
+                this.setState({
+                    direction: 'right'
+                })
+            }
+        } 
     }
 
     render(){
